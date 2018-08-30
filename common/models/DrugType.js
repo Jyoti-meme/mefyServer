533f7f08a7c2158fe1097e40189d77a88b883b04
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-08-30 14:28:04 +0530

    old code added

--- common/models/DrugType.js
+++ common/models/DrugType.js
@@ -1,7 +1,22 @@
 'use strict';
+// const Composer = require('../lib/composer.js');
+// const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+// const bizNetworkConnection = new BusinessNetworkConnection();
+// const cardName = "admin@mefy";
 
-const Composer = require('../lib/composer.js');
+module.exports = function (Drugtype) {
 
-module.exports = function(DrugType) {
-  Composer.restrictModelMethods(DrugType);
+    /** alter type and trim to get typeId */
+    Drugtype.beforeRemote('create', function (context, drugtype, next) {
+        let drugtypeId;
+        if (context.args.data.type.includes('')) {
+            drugtypeId = context.args.data.type.replace(/ +/g, "");
+        }
+        else {
+            drugtypeId = context.args.data.type;
+        }
+        context.args.data.typeId = drugtypeId.toLowerCase();
+        next();
+    });
+
 };
