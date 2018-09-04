07a3b11a7d752a6dc61fa2409dffba7c51b1b132
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-09-04 17:06:45 +0530

    user creation remote method made

--- common/models/User.js
+++ common/models/User.js
@@ -4,7 +4,7 @@ const Composer = require('../lib/composer.js');
 const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
 const bizNetworkConnection = new BusinessNetworkConnection();
 const cardName = "admin@mefy";
-
+const app=require('../../server/server');
 module.exports = function (User) {
 
   /***CHECK THE EXISTENCE OF EMAIL AND PHONENUMBER */
@@ -13,16 +13,16 @@ module.exports = function (User) {
 
 
   /*** AFTER REMOTE METHOD  */
-  User.afterRemote('**', function (context, user, next) {
-    var status = context.res.statusCode;
-    context.result = {
-      error: 'false',
-      users: context.result,
-      message: context.methodString + " success"
-    }
+  // User.afterRemote('**', function (context, user, next) {
+  //   var status = context.res.statusCode;
+  //   context.result = {
+  //     error: 'false',
+  //     users: context.result,
+  //     message: context.methodString + " success"
+  //   }
 
-    next();
-  })
+  //   next();
+  // })
 
   /*** CATCH ERORRS */
   User.afterRemoteError('create', function (ctx, next) {
@@ -55,9 +55,69 @@ module.exports = function (User) {
   //   return userData;
   // }
 
-  User.remoteMethod('addPharmacy', {
-    accepts: { arg: 'phoneNumber', type: 'string' },
-    returns: { arg: 'phoneNumber', type: 'string' }
-  });
+  User.registration = function (phoneNumber, role, name, gender, dob, city, deviceId, cb) {
+    console.log('USER', phoneNumber, role)
+    const Individual = app.models.individual;
+    User.find({ where: { phoneNumber: phoneNumber } }, function (err, user) {
+      console.log('user info', user)
+      if (user.length != 0) {
+        var response = {
+          error: false,
+          message: 'User alresdy registered'
+        }
+        cb(null, response)
+        
+      }
+      else {
+        User.create(
+          {
+              phoneNumber: phoneNumber,
+              role: role,
+          }, function(err, res) {
+            console.log('user created response',res)
+            // send otp and verify it then create individual
+            Individual.create({
+              name:name,
+              phoneNumber:phoneNumber,
+              gender:gender,
+              dob:dob,
+              city:city,
+              deviceId:deviceId,
+              userId:res.userId
+            },function(err,res){
+              console.log('RESPONSEEE',res)
+              console.log('errrror',err)
+              var sucresponse={
+                error:false,
+                user:res,
+                message:'User created Successfully'
+              }
+              cb(null, err || sucresponse);
+            })
+           
+              // cb(null);
+          }
+      );
+        
+      }
+    });
+   
+  }
+
+  User.remoteMethod('registration', {
+    http: { path: '/registration', verb: 'post' },
+    accepts: [
+      { arg: 'phoneNumber', type: 'string' },
+      { arg: 'role', type: 'string' },
+      { arg: 'name', type: 'string' },
+      { arg: 'gender', type: 'string' },
+      { arg: 'dob', type: 'string' },
+      { arg: 'city', type: 'string' },
+      { arg: 'deviceId', type: 'string' }
+    ],
+    //   , ],
+    returns: { arg: 'result', type: 'string' },
+  },
+  );
 
 };
