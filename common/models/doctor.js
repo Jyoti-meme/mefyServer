6b40ef39926839e34d16e227f7b90d8fc8eeddbf
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-09-10 15:13:09 +0530

    UPDATE API MADE

--- common/models/doctor.js
+++ common/models/doctor.js
@@ -1,7 +1,49 @@
 'use strict';
 
 const Composer = require('../lib/composer.js');
+const server = require('../../server/server');
 
-module.exports = function(doctor) {
+module.exports = function (doctor) {
   // Composer.restrictModelMethods(doctor);
+
+  /**********************************************DOCTOR UPDATE API******************** */
+
+  doctor.remoteMethod('updateProfile', {
+    http: { path: '/profile/:userId', verb: 'put' },
+    accepts: [
+      { arg: 'userId', type: 'string', required: true, http: { source: 'path' } },
+      { arg: 'data', type: 'obj' }
+    ],
+    returns: { arg: 'result', type: 'string' },
+  });
+
+  // UPDATE DOCTOR PROFILE WITH PASSED FIELDS
+  doctor.updateProfile = function (userId, data, cb) {
+    //check user existence
+    doctor.findOne({ where: { userId: 'resource:io.mefy.commonModel.User#' + userId } }, function (err, exists) {
+      console.log('result', exists)
+      if (exists != null && Object.keys(exists).length != 0) {
+        // update attributes
+        exists.updateAttributes(data, function (err, result) {
+          console.log('response from update', result);
+
+          let success = {
+            error: false,
+            result: result,
+            message: 'Profile updated Sucessfully'
+          }
+          cb(null, success);
+        })
+
+      }
+      else {
+        let error = {
+          error: true,
+          message: 'User not found'
+        }
+        cb(null, error)
+      }
+    })
+  }
+  /******************************************************************************************* */
 };
--- common/models/individual.js
+++ common/models/individual.js
@@ -2,6 +2,49 @@
 
 const Composer = require('../lib/composer.js');
 
-module.exports = function(individual) {
-  Composer.restrictModelMethods(individual);
+module.exports = function (individual) {
+
+
+  /*********************UPDATE INDIVIDUAL****************************** */
+
+  individual.remoteMethod('updateProfile', {
+    http: { path: '/profile/:userId', verb: 'put' },
+    accepts: [
+      { arg: 'userId', type: 'string', required: true, http: { source: 'path' } },
+      { arg: 'data', type: 'obj' }
+    ],
+    returns: { arg: 'result', type: 'string' },
+  });
+
+
+  // UPDATE INDIVIDUAL PROFILE WITH PASSED FIELDS
+  individual.updateProfile = function (userId, data, cb) {
+    //check user existence
+    individual.findOne({ where: { userId: 'resource:io.mefy.commonModel.User#' + userId } }, function (err, exists) {
+      console.log('result', exists)
+      if (exists != null && Object.keys(exists).length != 0) {
+        // update attributes
+        exists.updateAttributes(data, function (err, result) {
+          console.log('response from update', result);
+
+          let success = {
+            error: false,
+            result: result,
+            message: 'Profile updated Sucessfully'
+          }
+          cb(null, success);
+        })
+
+      }
+      else {
+        let error = {
+          error: true,
+          message: 'User not found'
+        }
+        cb(null, error)
+      }
+    })
+  }
+
+  /******************************************************************** */
 };
