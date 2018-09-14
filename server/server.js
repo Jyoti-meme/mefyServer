d63ed856a9fae89a266a66634acd1165c183cee1
meme-neha <neha.keshri@memeinfotech.com>
2018-09-14 12:52:15 +0530

    user availability

--- server/server.js
+++ server/server.js
@@ -16,12 +16,13 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
 
-app.start = function() {
+
+
+app.start = function () {
   // start the web server
-  return app.listen(function() {
+  return app.listen(function () {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
@@ -31,45 +32,83 @@ app.start = function() {
     }
   });
 };
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
 
   // start the server if `$ node server.js`
-  if (require.main === module){
+  if (require.main === module) {
     // app.start();   
 
-/****       SOCKET CONNECTION        **** */
+    /****       SOCKET CONNECTION        **** */
     app.io = require('socket.io')(app.start());
-  // require('socketio-auth')(app.io, {
-  //   authenticate: function (socket, value, callback) {
+    // require('socketio-auth')(app.io, {
+    //   authenticate: function (socket, value, callback) {
 
-  //       var AccessToken = app.models.AccessToken;
-  //       //get credentials sent by the client
-  //       var token = AccessToken.find({
-  //         where:{
-  //           and: [{ userId: value.userId }, { id: value.id }]
-  //         }
-  //       }, function(err, tokenDetail){
-  //         if (err) throw err;
-  //         if(tokenDetail.length){
-  //           callback(null, true);
-  //         } else {
-  //           callback(null, false);
-  //         }
-  //       }); //find function..    
-  //     } //authenticate function..
-  // });
-  app.io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-      });
+    //       var AccessToken = app.models.AccessToken;
+    //       //get credentials sent by the client
+    //       var token = AccessToken.find({
+    //         where:{
+    //           and: [{ userId: value.userId }, { id: value.id }]
+    //         }
+    //       }, function(err, tokenDetail){
+    //         if (err) throw err;
+    //         if(tokenDetail.length){
+    //           callback(null, true);
+    //         } else {
+    //           callback(null, false);
+    //         }
+    //       }); //find function..    
+    //     } //authenticate function..
+    // });
+    app.io.on('connection', function (socket) {
+      console.log('a user connected');
+      socket.on('disconnect', function () {
+        console.log('user disconnected', socket.id);
+
+         new Promise(() => {
+          app.models.doctor.findOne({ where: { socketId: socket.id } }, function (err, exists) {
+            console.log('Doctor Socket Id...', exists)
+            if (exists != null && Object.keys(exists).length != 0) {
+              exists.updateAttribute({ 'availability': 'Offline' }, function (err, result) {
+                console.log('resultttt', result)
+                let successmessage = {
+                  error: false,
+                  result: result,
+                  message: 'Now User is offline'
+                }
+                return successmessage
+              })
+
+            } else {
+              app.models.individual.findOne({ where: { socketId: socket.id } }, function (err, exists) {
+                console.log('Individual Socket Id...', exists)
+                exists.updateAttribute({ 'availability': 'Offline' }, function (err, result) {
+                  console.log('resultttt', result)
+                  let successmessage = {
+                    error: false,
+                    result: result,
+                    message: 'Now User is offline'
+                  }
+                  return successmessage
+                })
+              })
+            }
+          })
         });
-      }
+
+      });
+    })
+
+    
+
+  }
 });
+
+
+
+
 
 
