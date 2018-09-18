8f6498f0d133bd4a33908141317d2d7f36531893
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-09-18 16:52:38 +0530

    clinic creation done

--- common/models/clinic.js
+++ common/models/clinic.js
@@ -3,11 +3,35 @@
 const Composer = require('../lib/composer.js');
 
 module.exports = function(clinic) {
-  const enabledRemoteMethods = ["findById", "create", "deleteById","find"];
+  const enabledRemoteMethods = ["findById", "create", "deleteById","find","addClinic"];
   clinic.sharedClass.methods().forEach(method => {
     const methodName = method.stringName.replace(/.*?(?=\.)/, '').substr(1);
     if (enabledRemoteMethods.indexOf(methodName) === -1) {
       clinic.disableRemoteMethodByName(methodName);
     }
   });
+
+  /***************ADD CLINIC********************************************** */
+  clinic.remoteMethod('addClinic', {
+    http: { path: '/addclinic', verb: 'post' },
+    accepts: { arg: 'data', type: 'object', http: { source: 'body' } },
+    returns: { arg: 'result', type: 'any' },
+  });
+
+
+
+  clinic.addClinic=function(data,cb){
+    clinic.create(
+      { data}, function (err, res) {
+        console.log('eesult',res)
+        let result={
+          error:false,
+          clinic:res,
+          message:"clinic create successfully"
+        }
+        cb(null,result);
+      })
+   
+  }
+  /************************************************************************ */
 };
