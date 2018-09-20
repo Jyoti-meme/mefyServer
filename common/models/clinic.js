25512f778cc4d0bad351ed52106ce4ac213e7d0f
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-09-20 13:17:06 +0530

    clinicby date method

--- common/models/clinic.js
+++ common/models/clinic.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const Composer = require('../lib/composer.js');
-
-module.exports = function(clinic) {
-  const enabledRemoteMethods = ["findById", "deleteById","find","addClinic","getClinicByDoctorId"];
+var moment = require('moment');
+module.exports = function (clinic) {
+  const enabledRemoteMethods = ["findById", "deleteById", "find", "addClinic", "getClinicByDoctorId"];
   clinic.sharedClass.methods().forEach(method => {
     const methodName = method.stringName.replace(/.*?(?=\.)/, '').substr(1);
     if (enabledRemoteMethods.indexOf(methodName) === -1) {
@@ -18,42 +18,117 @@ module.exports = function(clinic) {
     returns: { arg: 'result', type: 'any' },
   });
 
- clinic.addClinic=function(data,cb){
+  clinic.addClinic = function (data, cb) {
     clinic.create(
-      { doctorId:data.doctorId,clinicName:data.clinicName,phoneNumber:data.phoneNumber,city:data.city,
-        address:data.address,pin:data.pin,fee:data.fee,weekDays:data.weekDays,bookingStatus:data.bookingStatus,
-        availability:data.availability
+      {
+        doctorId: data.doctorId, clinicName: data.clinicName, phoneNumber: data.phoneNumber, city: data.city,
+        address: data.address, pin: data.pin, fee: data.fee, weekDays: data.weekDays, bookingStatus: data.bookingStatus,
+        availability: data.availability
       }, function (err, res) {
-        console.log('eesult',res)
-        let result={
-          error:false,
-          clinic:res,
-          message:"clinic create successfully"
+        console.log('eesult', res)
+        let result = {
+          error: false,
+          clinic: res,
+          message: "clinic create successfully"
         }
-        cb(null,result);
+        cb(null, result);
       })
-   
+
   }
-  /************************************************************************ */
-   /***************GET  CLINIC BY DOCTOR ID********************************************** */
-   clinic.remoteMethod('getClinicByDoctorId', {
+  /******************************* END ***************************************** */
+  /***************GET  CLINIC BY DOCTOR ID********************************************** */
+  clinic.remoteMethod('getClinicByDoctorId', {
     http: { path: '/clinicByDoctorId/:doctorId', verb: 'get' },
     description: " Get Clinic By DoctorId",
-    accepts: { arg: 'doctorId', type: 'string', http: { source: 'path' }},
+    accepts: { arg: 'doctorId', type: 'string', http: { source: 'path' } },
     returns: { arg: 'result', type: 'string' },
   });
 
   /*********************** LOGIC************************ */
-  clinic.getClinicByDoctorId = function(doctorId,cb){
-    clinic.find({ where:  { doctorId: 'resource:io.mefy.doctor.doctor#' + doctorId } }, function (err, result){
-      console.log('exists',result)
+  clinic.getClinicByDoctorId = function (doctorId, cb) {
+    clinic.find({ where: { doctorId: 'resource:io.mefy.doctor.doctor#' + doctorId } }, function (err, result) {
+      console.log('exists', result)
       let doctorClinc = {
         error: false,
         result: result,
         message: "Getting doctor's Clinic list"
       }
       cb(null, doctorClinc)
-  })
-}
-}
+    })
+  }
+
   /**********************************END************************************** */
+
+  /*********************CLINIC BY DATE AND DOCTORID*********************************** */
+  clinic.remoteMethod('clinicByDate', {
+    http: { path: '/clinicbydate', verb: 'get' },
+    description: " Get Clinic By DoctorId",
+    accepts: [{ arg: 'doctorId', type: 'string' }, { arg: 'date', type: 'string' }],
+    returns: { arg: 'result', type: 'string' },
+  })
+
+  clinic.clinicByDate = function (doctorId, date, cb) {
+    var day = moment(date).format('dddd')
+    console.log('day', day);
+    clinic.find({ where: { 'doctorId': 'resource:io.mefy.doctor.doctor#' + doctorId } }, function (err, res) {
+      // console.log('deb', res);
+     
+      ProcessArray(res,day).then(clinics => {
+        console.log('clinic list', clinics)
+        cb(null.clinics)
+      }).catch(err => {
+        console.log('err',err)
+        cb(null, err)
+      })
+    })
+
+
+
+
+
+    async function ProcessArray(array,day) {
+      // console.log('INSIDE PROCESS ARRAY', array)
+      console.log('DAY VALUE',day)
+      const x = [];
+      for (const subs of array) {
+        // console.log('substitute data',subs)
+        for(const list of subs.weekDays){
+          console.log('LISTTTTTT::',list)
+          await Promise.all([substitutedata(list.day,day)]).then(function (values) {
+            console.log('RETUNED VALUESSSS',values);
+            x.push(values[0]);
+          });
+        }
+     
+      }
+      return x;
+    }
+
+    async function substitutedata(item,day) {
+      console.log('INSIDE SUBSTITIUE FUNCTION', item+'DAY ALSO'+day)
+      return new Promise((resolve,reject) => {
+       
+        if(item==day){
+          console.log('INSIDE IF')
+          resolve(item)
+        }
+        else{
+          console.log('INDSIDE ELSE');
+        reject(false)
+        }
+        // clinic.findOne({ where: { individualId: item.individual.split('#')[1] } }, function (err, medicine) {
+        //   // console.log('INSIDE MEDICINE FIND METHOD', medicine[0])
+        //   console.log('mdecine', medicine)
+        //   resolve(medicine);
+        // })
+      })
+    }
+
+  }
+
+  /********************************************************************************** */
+}
+
+// standard date format 2018-09-19T14:23:38+05:30
+// {where:{and:[{'doctorId':'resource:io.mefy.doctor.doctor#'+doctorId},{"weekDays.day":"Monday"}]}}
+// {"weekDays.0.day":{inq:['Monday']}}
