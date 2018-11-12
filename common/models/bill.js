32ee900a45d90e503ae483e096dfd8cf2462c66f
meme-neha <neha.keshri@memeinfotech.com>
2018-11-12 16:38:02 +0530

    get bill list

--- common/models/bill.js
+++ common/models/bill.js
@@ -3,7 +3,7 @@
 const Composer = require('../lib/composer.js');
 
 module.exports = function (bill) {
-  const enabledRemoteMethods = ["findById", "deleteById", "find", "createBill"];
+  const enabledRemoteMethods = ["findById", "deleteById", "find", "createBill", "getBillById"];
   bill.sharedClass.methods().forEach(method => {
     const methodName = method.stringName.replace(/.*?(?=\.)/, '').substr(1);
     if (enabledRemoteMethods.indexOf(methodName) === -1) {
@@ -38,4 +38,52 @@ module.exports = function (bill) {
 
   }
   /********************************************END************************************************* */
+  /******************************************GET BILL BY DOCTORID OR INDIVIDUALID***********************/
+  bill.remoteMethod('getBillById', {
+    http: { path: '/getBillById', verb: 'get' },
+    description: "Get Bill by DoctorId or IndividulaId",
+    accepts: [{ arg: 'doctorId', type: 'string' }, { arg: 'individualId', type: 'string' }],
+    returns: { arg: 'result', type: 'any' }
+  });
+  bill.getBillById = function (doctorId, individualId, cb) {
+    if (doctorId != null) {
+      bill.find({ where: { doctorId: 'resource:io.mefy.doctor.doctor#' + doctorId } }, function (err, exists) {
+        console.log(' Doctor exists', exists)
+
+        if (exists != null && Object.keys(exists).length != 0) {
+          console.log(' Doctor exists', exists)
+          let billList = {
+            error: false,
+            result: exists,
+            message: "Get All Doctor's Bill List"
+          }
+          cb(null, billList)
+        }else {
+          let msg = {
+            message: 'Not Available'
+          }
+          cb(null, msg)
+        }
+      })
+
+    } else {
+      bill.find({ where: { individualId: 'resource:io.mefy.individual.individual#' + individualId } }, function (err, exists) {
+        if (exists != null && Object.keys(exists).length != 0) {
+          console.log(' individual exists', exists)
+          let billList = {
+            error: false,
+            result: exists,
+            message: "Get All Individual's Bill List"
+          }
+          cb(null, billList)
+        }else {
+          let msg = {
+            message: 'Not Available'
+          }
+          cb(null, msg)
+        }
+      })
+    }
+  }
+  /****************************************END***************************************** */
 };
