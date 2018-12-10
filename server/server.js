36df4867c4f8bc41ff45d475621a61624c121f22
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-12-10 14:29:07 +0530

    socket disconnect code changed

--- server/server.js
+++ server/server.js
@@ -70,22 +70,26 @@ boot(app, __dirname, function (err) {
         console.log('user disconnected', socket.id);
         
 
-        new Promise(() => {
+        // new Promise(() => {
     
           app.models.doctor.findOne({ where: { socketId: socket.id } }, function (err, exists) {
             console.log('Doctor Socket Id...', exists)
             if (exists != null && Object.keys(exists).length != 0) {
               exists.updateAttribute({ 'availability': 'Offline' }, function (err, result) {
                 console.log('resultttt', result)
-                let successmessage = {
-                  error: false,
-                  result: result,
-                  message: 'Now User is offline'
-                }
-                return successmessage
+                // let successmessage = {
+                //   error: false,
+                //   result: result,
+                //   message: 'Now User is offline'
+                // }
+                // return successmessage
+                socket.to(socket.id).emit('Doctor went offline');
               })
 
             } 
+            else{
+              console.log('DOCTOR WITH THAT SOCKET NOT FOUND')
+            }
           // else {
           //     // exist
           //     app.models.individual.findOne({ where: { socketId: socket.id } }, function (err, exists) {
@@ -111,7 +115,7 @@ boot(app, __dirname, function (err) {
           //     })
           //   }
           })
-        });
+        // });
 
       });
     })
